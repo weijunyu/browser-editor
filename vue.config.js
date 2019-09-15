a841d0baf3fb7a57e4fe3b8dbbf11861c064643f
Wei Junyu <weijunyu@users.noreply.github.com>
2019-09-15 21:24:30 +0800

    Removed inline html config, removed docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-let production = process.env.NODE_ENV === 'production';
 module.exports = {
-    publicPath: '',
+    publicPath: process.env.NODE_ENV === 'production' ? '/browser-editor/' : '/',
     configureWebpack: config => {
         config.module.rules.push(
             {
@@ -16,20 +14,5 @@ module.exports = {
                 ]
             }
         )
-    },
-    chainWebpack: config => {
-        // Use webpack-chain to modify existing html-webpack-plugin
-        if (production) {
-            config
-                .plugin('html')
-                .tap(args => {
-                    args[0].inlineSource = '.js$';
-                    return args
-                })
-            // html-inline-source inlines js files into the output html
-            config
-                .plugin('html-inline-source')
-                .use(HtmlWebpackInlineSourcePlugin)
-        }
     }
 }
