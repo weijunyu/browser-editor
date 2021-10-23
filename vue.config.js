601ceb9721e0bc52776c81dbbcaba96089b3845f
C4BEWGuSO9A <C4BEWGuSO9A@users.noreply.github.com>
2021-10-24 01:01:14 +0800

    Prod bundles webfonts for icons into app file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-const production = process.env.NODE_ENV === 'production';
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const production = process.env.NODE_ENV === "production";
 module.exports = {
   publicPath: process.env.PUBLIC_PATH || "/",
   configureWebpack: (config) => {
@@ -18,10 +18,19 @@ module.exports = {
   chainWebpack: (config) => {
     // Use webpack-chain to modify existing html-webpack-plugin
     if (production) {
+      // module.rules[?].test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+      // For web fonts used for icons, inline into data-urls.
+      config.module
+        .rule("fonts")
+        .use("url-loader")
+        .tap((options) => ({ limit: undefined, fallback: undefined }));
+
+      // HtmlWebpackPlugin
       config.plugin("html").tap((args) => {
         args[0].inlineSource = ".(js|css|woff|woff2|ttf)$";
         return args;
       });
+
       // html-inline-source inlines js files into the output html
       config.plugin("html-inline-source").use(HtmlWebpackInlineSourcePlugin);
     }
